930de2683c7948bd5d9ad29773c7ff40ab39e8fe
bgpark <bgpark82@gmail.com>
2021-02-14 14:50:48 +0900

    fix: add notificatio test

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 import Todos from "./components/Todos.js";
 import Input from "./components/Input.js";
 
-// Let's check if the browser supports notifications
-if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-}
-
-// Let's check whether notification permissions have already been granted
-else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    console.log(Notification.permission);
-    var notification = new Notification("Hi there!");
-}
-
-// Otherwise, we need to ask the user for permission
-else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-            var notification = new Notification("Hi there!");
-        }
-    });
-}
-
 new (function () {
     this.init = () => {
         this.state = {
